41c2bdb2b0c220fefc14f1adb454f1806a210889
Msalam96 <34826407+Msalam96@users.noreply.github.com>
2019-06-02 14:15:44 -0400

    First Commit

---
+++ data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+
+stats = pd.read_csv('Seasons_Stats.csv', sep=',')
+stats.head()
